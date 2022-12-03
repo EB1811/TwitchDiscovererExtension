102a92002878efe996d860feebc35dee417f005d
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2022-12-03 13:01:57 +0000

    Category Select component

--- twitch-streamer-discoverer-extension/src/components/Overlay.svelte
+++ twitch-streamer-discoverer-extension/src/components/Overlay.svelte
@@ -1,26 +1,18 @@
 <script lang="ts">
-    import { storage } from "src/storage";
-    import { onMount } from "svelte";
-    import Options from "./Options.svelte";
-
-    let count = 0;
-
-    onMount(() => {
-        storage.get().then((storage) => (count = storage.count));
-    });
+  import ExtensionApp from './ExtensionApp.svelte'
 </script>
 
 <div class="overlay">
-    <Options {count} />
+  <ExtensionApp />
 </div>
 
 <style>
-    .overlay {
-        position: fixed;
-        top: 16px;
-        left: 16px;
-        background-color: white;
-        border: 1px solid black;
-        padding: 4px;
-    }
+  .overlay {
+    position: fixed;
+    top: 16px;
+    left: 16px;
+    background-color: white;
+    border: 1px solid black;
+    padding: 4px;
+  }
 </style>
