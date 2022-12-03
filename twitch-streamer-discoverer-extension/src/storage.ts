dc7d73551c8687d8385067d2bccc195a126ce68a
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2022-12-03 13:00:24 +0000

    removed defaults

--- twitch-streamer-discoverer-extension/src/storage.ts
+++ twitch-streamer-discoverer-extension/src/storage.ts
@@ -1,13 +1,19 @@
+import type {ApiCategoryItem} from './components/functions/getApiCategories'
+
 type IStorage = {
-    count: number;
-};
+  viewersMin?: number
+  viewersMax?: number
+  chosenCategories?: ApiCategoryItem[]
+}
 
 const defaultStorage: IStorage = {
-    count: 0,
-};
+  viewersMin: 0,
+  viewersMax: 0,
+  chosenCategories: []
+}
 
 export const storage = {
-    get: (): Promise<IStorage> =>
-        chrome.storage.sync.get(defaultStorage) as Promise<IStorage>,
-    set: (value: IStorage): Promise<void> => chrome.storage.sync.set(value),
-};
+  get: (): Promise<IStorage> =>
+    chrome.storage.sync.get(defaultStorage) as Promise<IStorage>,
+  set: (value: IStorage): Promise<void> => chrome.storage.sync.set(value)
+}
