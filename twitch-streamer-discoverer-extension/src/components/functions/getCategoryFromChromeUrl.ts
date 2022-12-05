f0b9ff226e9b4d2e56764c7476c8c6c0fc04290c
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2022-12-05 12:22:38 +0000

    Function to get category from api using url

---
+++ twitch-streamer-discoverer-extension/src/components/functions/getCategoryFromChromeUrl.ts
@@ -0,0 +1,28 @@
+import type {ApiCategoryItem} from './getApiCategoriesByName'
+import getApiCategoriesByName from './getApiCategoriesByName'
+
+const getCategoryFromChromeUrl = async (
+  bearerToken: string
+): Promise<ApiCategoryItem | undefined> => {
+  console.log('getting category from chrome url')
+
+  const tabUrl: string | undefined = (
+    await chrome.tabs.query({active: true, lastFocusedWindow: true})
+  )[0]?.url
+  if (!tabUrl?.includes('twitch.tv/directory/game/')) return
+
+  // 🙃
+  const categoryName: string = decodeURIComponent(
+    tabUrl.split('twitch.tv/directory/game/')[1]?.split('?')[0] ?? ''
+  )
+  if (!categoryName) return
+  console.log('categoryName', categoryName)
+
+  const category: ApiCategoryItem | undefined = (
+    await getApiCategoriesByName(bearerToken, categoryName)
+  ).find((category: ApiCategoryItem) => category.name === categoryName)
+
+  return category
+}
+
+export default getCategoryFromChromeUrl
