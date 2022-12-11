22263753ef53f08cfc1a11c2d61684f5474d7e2b
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2022-12-11 14:11:13 +0000

    More options + dev mode + storage

--- twitch-streamer-discoverer-extension/src/components/PostTokenApp.svelte
+++ twitch-streamer-discoverer-extension/src/components/PostTokenApp.svelte
@@ -24,7 +24,7 @@
   let errorMessage: string = ''
 
   let countdownSeconds: number
-  let newStreamTimer: ReturnType<typeof setTimeout>
+  let newStreamTimer: ReturnType<typeof setTimeout> | undefined
 
   // ? Put this in a function.
   onMount(async () => {
@@ -36,8 +36,12 @@
           ...chosenCategories,
           ...storedOptions.chosenCategories
         ]
+      if (storedOptions.showAdvancedOptions) showAdvancedOptions = true
+      if (storedOptions.mode) mode = storedOptions.mode
       if (storedOptions.viewersMin) viewersMin = storedOptions.viewersMin
       if (storedOptions.viewersMax) viewersMax = storedOptions.viewersMax
+      if (storedOptions.wantedCountdownTime)
+        wantedCountdownTime = storedOptions.wantedCountdownTime
     }
     setOptionsFromStorage()
   })
@@ -45,14 +49,15 @@
   onMount(() => {
     const addCategoryFromUrl = async () => {
       try {
-        const {bearerToken} = await storage.get()
+        const {bearerToken, chosenCategories: storedCategories} =
+          await storage.get()
         if (!bearerToken) {
           isAuthorized = false
           return
         }
 
         const urlCategory: ApiCategoryItem | undefined =
-          await getCategoryFromChromeUrl(bearerToken)
+          await getCategoryFromChromeUrl(bearerToken, storedCategories)
 
         if (
           urlCategory &&
@@ -70,16 +75,31 @@
     addCategoryFromUrl()
   })
 
+  const validate = (): void => {
+    if (chosenCategories.length <= 0) {
+      errorMessage = 'Select at least one category'
+      randomStreamUrl = ''
+      return
+    }
+    errorMessage = ''
+  }
+
   // TODO: Put timer in own component
   const startFindStreamCountdown = (): void => {
     resetNewStreamIntervalLoop()
-
+    randomStreamUrl = ''
+    setRandomStreamUrl()
     newStreamTimer = setTimeout(startNewStreamIntervalLoop, 1000)
   }
 
+  const extendFindStreamCountdown = (): void => {
+    if (countdownSeconds < 10000) countdownSeconds += wantedCountdownTime
+  }
+
   const startNewStreamIntervalLoop = async (): Promise<void> => {
+    if (errorMessage) return
+
     countdownSeconds -= 1
-    console.log('TIME:', countdownSeconds)
 
     if (countdownSeconds > 0) {
       newStreamTimer = setTimeout(startNewStreamIntervalLoop, 1000)
@@ -90,8 +110,13 @@
     }
   }
 
-  const pauseNewStreamIntervalLoop = (): void => {
-    clearTimeout(newStreamTimer)
+  const pauseResumeNewStreamIntervalLoop = (): void => {
+    if (newStreamTimer) {
+      clearTimeout(newStreamTimer)
+      newStreamTimer = undefined
+    } else {
+      newStreamTimer = setTimeout(startNewStreamIntervalLoop, 1000)
+    }
   }
 
   const resetNewStreamIntervalLoop = (): void => {
@@ -100,59 +125,93 @@
   }
 
   const setRandomStreamUrl = async (): Promise<void> => {
-    console.log('RUUUUUUUUUUUUUUUN')
-    // if (chosenCategories.length <= 0) {
-    //   errorMessage = 'Select at least one category'
-    //   randomStreamUrl = ''
-    //   return
-    // }
-    // errorMessage = ''
+    validate()
+    if (errorMessage) return
 
-    // try {
-    //   const {bearerToken} = await storage.get()
-    //   if (!bearerToken) {
-    //     isAuthorized = false
-    //     return
-    //   }
+    try {
+      const {bearerToken} = await storage.get()
+      if (!bearerToken) {
+        isAuthorized = false
+        return
+      }
 
-    //   randomStreamUrl = await getRandomStreamUrl({
-    //     bearerToken,
-    //     viewersMin,
-    //     viewersMax,
-    //     categories: chosenCategories
-    //       .filter(category => category.id)
-    //       .map(category => category.id!),
-    //     languages: ['en']
-    //   })
-    //   chrome.tabs.update({
-    //     url: randomStreamUrl
-    //   })
-    //   // getApiCategoriesByName(bearerToken, 'pokemon')
-    //   // getApiCategories(bearerToken)
-    // } catch (error) {
-    //   console.log('new ERROR', error)
-    //   errorMessage =
-    //     error instanceof Error ? error.message : 'Error from server'
-    //   randomStreamUrl = ''
-    // }
+      const streamUrl = await getRandomStreamUrl({
+        bearerToken,
+        viewersMin,
+        viewersMax,
+        categories: chosenCategories
+          .filter(category => category.id)
+          .map(category => category.id!),
+        languages: ['en']
+      })
+
+      if (mode.includes('LINK')) randomStreamUrl = streamUrl
+      if (mode.includes('GOTO'))
+        chrome.tabs.update({
+          url: streamUrl
+        })
+    } catch (error) {
+      console.log('new ERROR', error)
+      errorMessage =
+        error instanceof Error ? error.message : 'Error from server'
+      randomStreamUrl = ''
+    }
   }
 </script>
 
 <div class="mt-1">
-  <button
-    class="w-full"
-    on:click={() => {
-      showAdvancedOptions = !showAdvancedOptions
-    }}
-  >
-    <hr class="w-full border-gray-500" />
-  </button>
+  <div class="">
+    <button
+      class="relative w-full border-t border-gray-600 p-1 transition-colors duration-100
+       hover:border-violet-500"
+      on:click={() => {
+        {
+          showAdvancedOptions = !showAdvancedOptions
+          storage.set({showAdvancedOptions})
+        }
+      }}
+    >
+      {#if !showAdvancedOptions}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="absolute left-0 right-0 top-0 m-auto h-3 w-3 rounded-lg text-gray-400"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+          />
+        </svg>
+      {:else}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="absolute left-0 right-0 top-0 m-auto h-3 w-3 rounded-lg text-gray-400"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M4.5 15.75l7.5-7.5 7.5 7.5"
+          />
+        </svg>
+      {/if}
+    </button>
+  </div>
   {#if showAdvancedOptions}
-    <div class="rounded bg-black p-2">
+    <div class="">
       <div class="my-1 flex">
         <select
           bind:value={mode}
-          class="rounded-md border border-neutral-700 bg-black px-2 py-1 text-gray-100"
+          on:change={() => storage.set({mode})}
+          class="flex-1 rounded-md border border-neutral-700 bg-black px-2 py-1 text-gray-100 
+          transition duration-300 focus:outline-none focus:ring-1 focus:ring-violet-500"
         >
           <option value="LINK">Show link to new stream</option>
           <option value="GOTO">Go to new stream</option>
@@ -165,7 +224,10 @@
               min="0"
               placeholder="Time"
               bind:value={wantedCountdownTime}
-              class="w-full rounded-md border border-neutral-700 bg-black py-1 pl-2 pr-7 text-neutral-100 placeholder-gray-500 transition duration-300 ease-in-out focus:border-transparent focus:outline-none focus:ring-1 focus:ring-violet-500"
+              on:input={() => storage.set({wantedCountdownTime})}
+              class="w-full rounded-md border border-neutral-700 bg-black py-1 pl-2 pr-7 text-neutral-100
+               placeholder-gray-500 transition duration-300 ease-in-out focus:border-transparent
+                focus:outline-none focus:ring-1 focus:ring-violet-500"
             />
             {#if wantedCountdownTime}
               <div
@@ -184,7 +246,10 @@
           placeholder="Min viewers"
           bind:value={viewersMin}
           on:input={() => storage.set({viewersMin})}
-          class="w-full rounded-md border border-neutral-700 bg-black px-2 py-1 text-neutral-100 placeholder-gray-500 transition duration-300 ease-in-out focus:border-transparent focus:outline-none focus:ring-1 focus:ring-violet-500"
+          class="w-full rounded-md border border-neutral-700 bg-black px-2 py-1 text-neutral-100
+           placeholder-gray-500 transition duration-300 ease-in-out focus:border-transparent focus:outline-none
+            focus:ring-1
+           focus:ring-violet-500 "
         />
         <input
           type="number"
@@ -192,7 +257,9 @@
           placeholder="Max viewers"
           bind:value={viewersMax}
           on:input={() => storage.set({viewersMax})}
-          class="ml-1 w-full rounded-md border border-neutral-700 bg-black px-2 py-1 text-neutral-100 placeholder-gray-500 transition duration-300 ease-in-out focus:border-transparent focus:outline-none focus:ring-1 focus:ring-violet-500"
+          class="ml-1 w-full rounded-md border border-neutral-700 bg-black px-2 py-1 text-neutral-100
+           placeholder-gray-500 transition duration-300 ease-in-out focus:border-transparent focus:outline-none
+            focus:ring-1 focus:ring-violet-500"
         />
       </div>
     </div>
@@ -203,7 +270,8 @@
     class="mt-5 flex flex-grow flex-col items-center justify-center py-2 lg:mt-6"
   >
     <button
-      class="rounded bg-violet-600 py-2 px-4 font-bold text-white transition-all duration-100 hover:bg-violet-700"
+      class="rounded bg-violet-600 py-2 px-4 font-bold text-white transition-all duration-100
+       hover:bg-violet-700"
       on:click={mode.includes('TIMER')
         ? startFindStreamCountdown
         : setRandomStreamUrl}>Get Random Stream</button
@@ -221,15 +289,23 @@
           >
         </h1>
       {:else if countdownSeconds}
-        <h1 class="text-lg font-bold text-gray-50">
-          {countdownSeconds}
-        </h1>
+        <div class="relative">
+          <button
+            on:click={pauseResumeNewStreamIntervalLoop}
+            class="text-lg font-bold text-gray-50 transition-colors duration-100 hover:text-blue-200"
+          >
+            {countdownSeconds}
+          </button>
+          <button
+            on:click={extendFindStreamCountdown}
+            class="absolute ml-1 text-lg font-bold text-gray-300 transition-colors duration-100
+             hover:text-blue-200"
+          >
+            +
+          </button>
+        </div>
       {/if}
     </div>
-    <!-- <button
-      class="rounded bg-red-600 py-2 px-4 font-bold text-white transition-all duration-100 hover:bg-red-700"
-      on:click={pauseNewStreamIntervalLoop}>Pause</button
-    > -->
   </div>
 </div>
 
