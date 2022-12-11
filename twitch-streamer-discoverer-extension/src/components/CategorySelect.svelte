8f72955b22eb077855a6a68bb1bd4e43ca7536bf
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2022-12-11 14:10:47 +0000

    class changes

--- twitch-streamer-discoverer-extension/src/components/CategorySelect.svelte
+++ twitch-streamer-discoverer-extension/src/components/CategorySelect.svelte
@@ -94,7 +94,9 @@
         id="search"
         type="text"
         placeholder="Search for a category"
-        class="block w-full rounded-md border border-neutral-700 bg-black py-2 pl-10 pr-3 leading-5 text-white placeholder-gray-500 transition duration-300 ease-in-out focus:border-transparent focus:outline-none focus:ring-1 focus:ring-violet-500 sm:text-sm"
+        class="block w-full rounded-md border border-neutral-700 bg-black py-2 pl-10 pr-3
+        leading-5 text-white placeholder-gray-500 transition duration-300 ease-in-out
+        focus:border-transparent focus:outline-none focus:ring-1 focus:ring-violet-500 sm:text-sm"
         bind:value={searchTerm}
         on:input={setSuggestionsFromApi}
       />
@@ -128,7 +130,8 @@
       {#each suggestedCategories as suggestion}
         <button class="w-full" on:click={() => addCategory(suggestion)}>
           <div
-            class="flex items-center justify-between bg-neutral-900 p-2 transition-colors duration-100 ease-in-out hover:bg-gray-800"
+            class="flex items-center justify-between bg-neutral-900 p-2 transition-colors duration-100
+            ease-in-out hover:bg-gray-800"
           >
             <span class="w-full text-left text-xs font-bold text-white"
               >{suggestion.name}
@@ -156,7 +159,8 @@
           )}
         >
           <div
-            class="inline-flex items-center rounded-full bg-violet-600 px-2 py-1 text-xs font-semibold text-gray-50 transition-all duration-100 hover:bg-violet-700"
+            class="inline-flex items-center rounded-full bg-violet-600 px-2 py-1 text-xs font-semibold
+            text-gray-50 transition-all duration-100 hover:bg-violet-700"
           >
             <span>{chosenCategory.name}</span>
             <span class="ml-1">
